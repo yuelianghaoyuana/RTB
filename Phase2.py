a8f86c09d26720cf3d357cc851afe51f2ae7d0a4
yuelianghaoyuana <yuelianghaoyuana@users.noreply.github.com>
2014-04-18 18:53:19 +0200

    Update Phase2.py

--- Phase2.py
+++ Phase2.py
@@ -2,16 +2,17 @@ import time,os
 import numpy as np
 import scipy as sp
 import sys
+import gensim
 
 
 from sklearn import linear_model
 from sklearn.cross_validation import train_test_split
 from sklearn.grid_search import GridSearchCV
 from sklearn.metrics import classification_report
-from sklearn.svm import SVC
 from sklearn.mixture import  GMM
 import cPickle
-from sklearn.naive_bayes import MultinomialNB
+import sklearn.naive_bayes  #MultinomialNB
+import sklearn.svm  #SVC
 
 def find_max_index(mylist):
 	maxV=-sys.maxint
@@ -42,7 +43,7 @@ def read_numbers_form_column(filePath):
 	return data
 
 
-def clutering_advs(filePathC,filePathW, algoCluster,numT=10):
+def clutering_advs(filePathC,filePathW, algoCluster,numT=10, drop=False):
 	#classes and weights
 	cls=read_numbers_form_column(filePathC)
 	wts=read_numbers_form_column(filePathW)
@@ -94,24 +95,24 @@ def clutering_advs(filePathC,filePathW, algoCluster,numT=10):
 	# import matplotlib.pyplot as plt
 	# plt.plot(list(range(1,numT+1)), scores,'go')
 	# plt.show()
-
+	if (drop):
 	#statistics of weights in each cluster
-	means=np.zeros(len_cluster)
-	for i in xrange(len_cluster):
-		means[i]=sum(clustersWt[i])/float(len(clustersWt[i]))
-	# print means
+		means=np.zeros(len_cluster)
+		for i in xrange(len_cluster):
+			means[i]=sum(clustersWt[i])/float(len(clustersWt[i]))
+		# print means
 
-	#delect the most big and most small
+		#delect the most big and most small
 
-	G=find_max_index(means)
-	S=find_max_index(-means)
+		G=find_max_index(means)
+		S=find_max_index(-means)
 
-	to_drop=[G,S]
+		to_drop=[G,S]
 
-	del clusters[G]
-	del clusters[S]
-	del clustersWt[G]
-	del clustersWt[S]
+		del clusters[G]
+		del clusters[S]
+		del clustersWt[G]
+		del clustersWt[S]
 	return clusterIds,clusters,clustersWt
 
 
@@ -120,12 +121,10 @@ def clutering_advs(filePathC,filePathW, algoCluster,numT=10):
 '''
 stock the matrix in scipy sparse matrix format, reduce from 600M to 12M
 '''
-
-foldPath='../data/matrix 2014_04_13'
-
 def change_full_to_sparse_and_stock(foldPath,pickleX,pickleY):
 	cTok=''
 	filenames=[]
+	usr_ids=[]
 	for file in os.listdir(foldPath):
 		if file.startswith('matrix'):
 			filenames.append(foldPath+'/'+file)
@@ -138,8 +137,25 @@ def change_full_to_sparse_and_stock(foldPath,pickleX,pickleY):
 
 	for file in filenames:
 		f=open(file)
-		f.readline()
+		
+		# store the md5
+		# -----------------------
+		if i!=0:
+			f.readline()
+		else:
+			md5=f.readline()
+			md5s= md5.strip().split()
+			#remove de string 'User_id','Score','CONV_id'
+			del md5s[0]
+			del md5s[-1]
+			del md5s[-1]
+		#--------------------------
+
+
 		data=np.loadtxt(f)
+
+		usr_ids.extend(data[:,0])
+
 		# y=  data[:,-1].astype(int).reshape(data.shape[0],1)
 		y=  data[:,-1].astype(int)
 
@@ -147,8 +163,8 @@ def change_full_to_sparse_and_stock(foldPath,pickleX,pickleY):
 		if i==0:
 			cTok= [j for j in xrange(data.shape[1])]
 			del cTok[-1]
+			del cTok[-1]
 			del cTok[0]
-
 		data= data[:, cTok]
 
 		# X.append(data)
@@ -158,21 +174,24 @@ def change_full_to_sparse_and_stock(foldPath,pickleX,pickleY):
 		#how many nb of domain he has each person has visited
 		#print np.sum(data,1)
 		#print data[0],y
-		print 'file ',i
+
 		if i!=0:
-			X= sp.sparse.vstack((X,sp.sparse.coo_matrix(data)))
+			X= sp.sparse.vstack((X,sp.sparse.csr_matrix(data).astype(int)))
 			Y= np.hstack((Y,y))
 		else:
-			X=sp.sparse.coo_matrix(data)
+			X=sp.sparse.csr_matrix(data).astype(int)
 			Y=y
 
 		# print X.shape
 		# print Y.shape
 		
-		print 'done stock petit '+ str(i)
+		print 'done small stock '+ str(i)
 		
 		i+=1
-
+		
+	#store the information: X, Y, usr_ids, md5(domain+deplacement)
+	cPickle.dump(usr_ids, open(pickleUsrids, "w"))
+	cPickle.dump(md5s, open(pickleMd5s, "w"))
 	cPickle.dump(X, open(pickleX, "w"))
 	cPickle.dump(Y, open(pickleY, "w"))
 
@@ -207,6 +226,7 @@ def change_full_to_stock(foldPath,pickleX,pickleY):
 		if i==0:
 			cTok= [j for j in xrange(data.shape[1])]
 			del cTok[-1]
+			del cTok[-1]
 			del cTok[0]
 
 		data= data[:, cTok]
@@ -234,6 +254,7 @@ def change_full_to_stock(foldPath,pickleX,pickleY):
 		i+=1
 
 
+
 	# cPickle.dump(X, open(pickleX, "w"))
 	# cPickle.dump(Y, open(pickleY, "w"))
 
@@ -248,46 +269,41 @@ def change_full_to_stock(foldPath,pickleX,pickleY):
 phase training algorithm of classification:
 '''
 
-# tuned_parameters = {'C':[0.01,0.1,1]}
-tuned_parameters = {'alpha':[0.1,1]}
-test_size=0.3
-score = 'precision'
-ppfile='post_porba.csv'
-
-
-
-def clf_return_score_and_proba(X_train, X_test, Y_train, Y_test,tuned_parameters,ppfile ):
-	
+def clf_return_score_and_proba(X,Y, test_size,tuned_parameters,ppfile,score,algo,cv):
+	X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=test_size, random_state=0)
 
 	print("# Tuning hyper-parameters for %s" % score)
 	print()
+	if cv>0:
+		clf = GridSearchCV(algo(), tuned_parameters, cv=cv, scoring=score,refit=True)
+		
+		st=time.time()
+		
+		clf.fit(X_train, Y_train)
 
-	clf = GridSearchCV(MultinomialNB(alpha=1), tuned_parameters, cv=5, scoring=score)
-	# clf=MultinomialNB(alpha=1)
-	st=time.time()
-	
-	clf.fit(X_train, Y_train)
+		st2=time.time()
+		
+		print 'train use: ', st2-st , ' time'
+		
+		print("Best parameters set found on development set:")
+		print(clf.best_estimator_)
 
-	st2=time.time()
-	
-	print 'train use: ', st2-st , ' time'
-	
-	# print("Best parameters set found on development set:")
-	# print()
-	# print(clf.best_estimator_)
-	# # print()
-	# print("Grid scores on development set:")
-	# print()
-	# for params, mean_score, scores in clf.grid_scores_:
-	#     print("%0.3f (+/-%0.03f) for %r"
-	#           % (mean_score, scores.std() / 2, params))
-	# print()
+		# print("Grid scores on development set:")
+		
+		for params, mean_score, scores in clf.grid_scores_:
+		    print("%0.3f (+/-%0.03f) for %r"
+		          % (mean_score, scores.std() / 2, params))
+	else:
+		clf=algo()
+		st=time.time()
+		clf.fit(X_train, Y_train)
+		st2=time.time()
 
 	# print("Detailed classification report:")
-	# print()
+	# 
 	# print("The model is trained on the full development set.")
 	# print("The scores are computed on the full evaluation set.")
-	# print()
+	
 	# y_true, y_pred = Y_test, clf.predict(X_test)
 
 	#print(classification_report(y_true, y_pred))
@@ -303,62 +319,161 @@ def clf_return_score_and_proba(X_train, X_test, Y_train, Y_test,tuned_parameters
 
 	perf= sum( pc==Y_test)/float(len(Y_test))
 	np.savetxt(ppfile, pp, delimiter=",")
-	return perf 
+
+	print pp
+
+	return pc,perf 
 
 
 
 '''
 phase clustering advertisers to regroup users in group:
 '''
-filenameC='all_classes.txt'
-filenameW='all_weights.txt'
-filePathC=foldPath+'/'+filenameC
-filePathW=foldPath+'/'+filenameW
 
+def find_key_if_value_in_dict(mydict, v):
+	res=''
+	for key, value in mydict.items():
+		if v in value:
+			res=key
+			break
+	return res
 
+def regroup_by_given_clusters(cl_dict, X,Y):
+	X_dict= {}
+	Y_dict= {}
 
+	list_id_per_cl={}
 
-# clusterIds,clusters,clustersWt= clutering_advs(filePathC,filePathW, algoCluster,numT=10)
-# print clusterIds
-# print clusters
-# print clustersWt
+	for cl_id in cl_dict:
+		list_id_per_cl[cl_id]=[] 
 
-
-
-
-
-# 
-
-
-# def regroup_by_given_clusters(clusters, X,Y):
-# 	X_dict= {}
-# 	Y_dict= {}
-
-# 	if len(X) != len(Y):
-# 		print 'error: X length not correspond Y length'
-# 		break
+	if X.shape[0] != len(Y):
+		print 'error: X length not correspond Y length'
 	
-# 	for i in xrange(len(X)):
-		
+	for i in xrange(len(Y)):
+		cl=find_key_if_value_in_dict(cl_dict,Y[i])
+		# print cl
+		if cl !='':
+			list_id_per_cl[cl].append(i)
 
+	# print list_id_per_cl
+
+	for cl_id in cl_dict:
+		#seperate X by row with index
+		X_dict[cl_id]=X[list_id_per_cl[cl_id],:]
+		Y_dict[cl_id]=Y[list_id_per_cl[cl_id],]
+
+
+	# print X_dict, Y_dict
+	return X_dict, Y_dict
+
+#@param: X: a sparse matrix format, for ex. csr
+#@return: X_red: reduced matrix
+#		  per_red: percentage reduced
+
+#problem: should i use X directly or another format
+def reduce_nonuseful_attr(X):
+	n,m=X.shape
+	#sum X in column:0, row:1
+	nonZeroCount=(X.sum(0)!=0)
+	indNonZero=np.asarray(nonZeroCount).reshape(-1)
+	
+
+	print len(indNonZero)
+
+	per_red= (m-sum(indNonZero))/float(m)
+
+	X_red= X[:,indNonZero]
+
+	print type(X_red), X_red.shape
+
+ 	return X_red,per_red
 
 if __name__=="__main__":
 
+	foldPath='C:/Users/tradelab/Documents/donnes/2014-04-07'
+	
 	pickleX=foldPath+'/X.pickle'
 	pickleY=foldPath+'/Y.pickle'
+	pickleMd5s=foldPath+'/md5s.pickle'
+	pickleUsrids=foldPath+'/usr_ids.pickle'
 
-	#comment it if we have already stocked the data in pickle format
-	change_full_to_sparse_and_stock(foldPath,pickleX,pickleY)
+	"""
+	----------------------store and read information----------------------------------------'
+	"""
+	#store info.  X is in sparse matrix format: coo
+	# change_full_to_sparse_and_stock(foldPath,pickleX,pickleY)
 
-	'''
-	reading the X and Y from cPickle 
-	'''
+	#reading info 
 	X = cPickle.load(open(pickleX))
-	Y= 	cPickle.load(open(pickleY))	
-	print X.shape, Y.shape
+	Y= 	cPickle.load(open(pickleY))
+	md5s = cPickle.load(open(pickleMd5s))
+	usr_ids=cPickle.load(open(pickleUsrids))
 
-	X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=test_size, random_state=0)
+	print 'X type is ', type(X)
 
-	pc,perf=clf_return_score_and_proba(X,Y, test_size,tuned_parameters,foldPath+'/'+ppfile)
-	print perf
-	# algoCluster=GMM(n_components=1, covariance_type='full',  random_state=None,n_iter=10000)
+	print 'Y type is ',	type(Y)
+	print 'X shape is ', X.shape
+	print 'Y shape is ', Y.shape
+	print 'mds length is ', len(md5s)
+	print 'usr_ids length is ',	len(usr_ids)
+
+	#important: when store, X is coo format
+	#			shold be changed to csr format for slicing
+	X=X.tocsr()
+	print 'change X type to ', type(X), ' for slicing'
+
+
+
+	"""
+	----------------------train and test----------------------------------------'
+	"""
+	# tuned_parameters = {'C':[0.01,0.1,1]}
+	tuned_parameters = {'alpha':[0.01,0.1,1,10,100]}
+	test_size=0.3
+	score = 'accuracy'
+	ppfile='post_porba.csv'
+	algo=getattr(sklearn.naive_bayes,'MultinomialNB')
+	pc,perf=clf_return_score_and_proba(X,Y, test_size,tuned_parameters,foldPath+'/'+ppfile,score,algo,cv=0)
+	
+	print 'perf with all data in a pool ', perf
+	
+	
+	"""
+	----------------------clustering----------------------------------------'
+	"""
+	filenameC='all_classes.txt'
+	filenameW='all_weights.txt'
+	filePathC=foldPath+'/'+filenameC
+	filePathW=foldPath+'/'+filenameW
+	algoCluster=GMM(n_components=1, covariance_type='full',  random_state=0,n_iter=10000)
+
+	clusterIds,clusters,clustersWt= clutering_advs(filePathC,filePathW, algoCluster,numT=10)
+	print clusterIds
+	print clusters
+	print clustersWt
+
+
+	"""
+	----------------------regroupment----------------------------------------'
+	"""
+	X_dict, Y_dict=regroup_by_given_clusters(clusters, X,Y)
+	per_red={}
+	for cl in X_dict:
+		print type(X_dict[cl])
+		X_dict[cl],per_red[cl]=reduce_nonuseful_attr(X_dict[cl])
+	print 'the matrix has reduced '
+	print per_red
+
+
+
+	pc_dict={}
+	perf_dict={}
+	for cl in X_dict:
+		print '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+		# print X_dict[cl],Y_dict[cl]
+		# print type(X_dict[cl]),X_dict[cl].shape
+		# print type(Y_dict[cl]),Y_dict[cl].shape
+		if cl==0:
+			pc_dict[cl],perf_dict[cl]=clf_return_score_and_proba(X_dict[cl],Y_dict[cl], test_size,tuned_parameters,ppfile,score,algo,cv=0)
+	print perf_dict
